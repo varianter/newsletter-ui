46370c646172cfcb0cf8ebf7141126e1ba8af36a
Ole Jørgen Skogstad <skogstad@softis.net>
2020-09-15 19:03:31 +0200

    --wip-- [skip ci]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { MsalService, BroadcastService } from '@azure/msal-angular';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'newsletter-ui';
+  constructor(private broadcastService: BroadcastService, private authService: MsalService) { }
+
+  ngOnInit() { }
+
+  login() {
+      const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+
+      if (isIE) {
+        this.authService.loginRedirect({
+          extraScopesToConsent: ["user.read", "openid", "profile"]
+        });
+      } else {
+        this.authService.loginPopup({
+          extraScopesToConsent: ["user.read", "openid", "profile"]
+        });
+      }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MsalModule, MsalInterceptor } from '@azure/msal-angular';
 
+const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule.forRoot({
+      auth: {
+        clientId: '6ca47c50-230f-4d6f-9075-42b87f364a4e',
+        authority: '0f16d077-bd82-4a6c-b498-52741239205f', 
+        redirectUri: 'http://localhost:4200'
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: isIE,
+      },
+    }, {
+      popUp: !isIE,
+      consentScopes: [
+        'user.read',
+        'openid',
+        'profile',
+      ],
+      unprotectedResources: [],
+      protectedResourceMap: [
+        ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+      ],
+      extraQueryParameters: {}
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
